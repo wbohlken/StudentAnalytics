5a83df00f7fde3fbe8e6743ad38c2f693c5e43f8
Justin Oud <justinoud@VSPs-MacBook-Air.local>
2015-08-27 21:43:26 +0200

    small change

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -149,7 +149,7 @@ class DashboardController extends Controller
                     $moodleResult->save();
                 }
             }
-            var_dump(shell_exec('sh /usr/share/kettle/data-integration/kitchen.sh -file=/usr/share/kettle/kettle_config/hva.kjb -param:CONFIG_DIR=/usr/share/kettle/kettle_config/ -param:weeknr='. $week_nr);
+            var_dump(shell_exec('sh /usr/share/kettle/data-integration/kitchen.sh -file=/usr/share/kettle/kettle_config/hva.kjb -param:CONFIG_DIR=/usr/share/kettle/kettle_config/ -param:weeknr='. $week_nr));
             die();
             // Return will return non-zero upon an error
             $oWeek->dashboard_created = 1;
