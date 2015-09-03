b0b3b91b0cf8fb6e3c91d2941fa98c17c25f4ecf
Justin Oud <justinoud@VSPs-MacBook-Air.local>
2015-09-03 21:48:12 +0200

    changed from address mail

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
 	|
 	*/
 
-	'from' => ['address' => 'oudj001@hva.nl', 'name' => 'Hogeschool van Amsterdam - Informatica'],
+	'from' => ['address' => 'heljf@@hva.nl', 'name' => 'Hogeschool van Amsterdam - Informatica'],
 
 	/*
 	|--------------------------------------------------------------------------
